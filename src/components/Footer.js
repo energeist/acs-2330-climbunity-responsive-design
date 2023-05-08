e2a1f3a474996402f6e149dcb6c534d03b34493e
Mark Rattle <mark.rattle@gmail.com>
2023-05-07 23:41:39 -0400

    navigation problem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,32 @@
-// src/Footer.js
 import React from 'react';
+import {useState} from 'react';
 import './Footer.css';
+import Modal from 'react-bootstrap/Modal';
+// import SubscriptionForm from './SubscriptionForm';
+
 
 function Footer() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <section className='Footer' aria-label="footer">
-      <p>Subscribe to our Newsletter!</p>
+      <a href="#" onClick={handleClick}>Subscribe to our Newsletter!</a>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Subscribe</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* <SubscriptionForm closeModal={handleClose} /> */}
+        </Modal.Body>
+      </Modal>
     </section>
   )
 }
